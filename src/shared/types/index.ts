03d10f70ee52f128333d5f39ab8b01c3c5752e2f
rowanex <artem23klyukin@gmail.com>
2024-02-20 14:09:56 +0500

    create cli application

---
+++ src/shared/types/index.ts
@@ -0,0 +1,8 @@
+export * from './user.type.js';
+export * from './offer.type.js';
+export * from './comment.type.js';
+export * from './house-type.enum.js';
+export * from './facilities.enum.js';
+export * from './coordinates.enum.js';
+export * from './city.enum.js';
+
