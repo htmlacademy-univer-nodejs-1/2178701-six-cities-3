d98c916be6f7f43ba6d1cf5c45a4f29cb5b50ecf
rowanex <artem23klyukin@gmail.com>
2024-03-05 23:03:48 +0500

    cli app part 2

---
+++ src/shared/libs/offer-generator/tsv-offer-generator.ts
@@ -0,0 +1,36 @@
+
+import { OfferGenerator } from './offer-generator.interface.js';
+import { MockServerData } from '../../types/index.js';
+import { getRandomItem, getRandomItems } from '../../helpers/index.js';
+
+
+export class TSVOfferGenerator implements OfferGenerator {
+  constructor(private readonly mockData: MockServerData) {}
+
+  public generate(): string {
+    const title = getRandomItem<string>(this.mockData.titles);
+    const description = getRandomItem<string>(this.mockData.descriptions);
+    const postDate = getRandomItem<string>(this.mockData.postDates);
+    const city = getRandomItem<string>(this.mockData.cities);
+    const previewPhoto = getRandomItem<string>(this.mockData.previewPhotos);
+    const photos = getRandomItems<string>(this.mockData.photos, 6).join(';');
+    const isPremium = getRandomItem<boolean>(this.mockData.isPremiums);
+    const isFavorite = getRandomItem<boolean>(this.mockData.isFavorites);
+    const rating = getRandomItem<number>(this.mockData.ratings);
+    const type = getRandomItem<string>(this.mockData.types);
+    const roomCount = getRandomItem<number>(this.mockData.roomCounts);
+    const guestsCount = getRandomItem<number>(this.mockData.guestsCounts);
+    const price = getRandomItem<number>(this.mockData.prices);
+    const facilities = getRandomItems<string>(this.mockData.facilities).join(';');
+    const author = getRandomItem<string>(this.mockData.authorNames);
+    const email = getRandomItem<string>(this.mockData.authorEmails);
+    const avatarPath = getRandomItem<string>(this.mockData.authorAvatars);
+    const coordinates = getRandomItem<string>(this.mockData.coordinates);
+
+    return [
+      title, description, postDate, city, previewPhoto, photos, isPremium,
+      isFavorite, rating, type, roomCount, guestsCount, price, facilities,
+      author, email, avatarPath, coordinates
+    ].join('\t');
+  }
+}
